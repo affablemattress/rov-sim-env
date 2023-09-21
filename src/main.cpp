928d1608490d7811813c399a54513d851e8fded9
Kayra Toprak SAY <kayratopraksay@gmail.com>
2023-09-22 00:57:21 +0300

    Add Dear IMGUI as a submodule and link statically.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,8 @@
 #include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <iostream>
 
 int main(int argc, char** args) {
