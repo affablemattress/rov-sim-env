dfbab5cca4c1ffd49a8ff0d2a31d22214a1aed9b
Kayra Toprak SAY <kayratopraksay@gmail.com>
2023-09-22 13:39:51 +0300

    Add spdlog as submodule and link statically.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,19 @@
-#include "glad/glad.h"
+#define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
+#include "glad/glad.h"
+#include "spdlog/spdlog.h"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 #include <iostream>
 
 int main(int argc, char** args) {
-    std::cout << "Hello World!, from main." << std::endl;
+    spdlog::set_level(spdlog::level::info);
+    spdlog::info("Hello world, from spdlog.");
+    
+    if(glfwInit() != GLFW_TRUE) {
+        spdlog::error("GLFW couldn't init.");
+    }
+
+    glfwTerminate();
 }
