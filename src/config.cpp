65254245f01dbe0600243761acc11d9f4d72b4ee
Kayra Toprak SAY <kayratopraksay@gmail.com>
2023-09-23 13:35:06 +0300

    Setup all libraries. Boilerplate for GLFW + GLAD + IMGUI + spdlog.

--- src/config.cpp
+++ src/config.cpp
@@ -2,8 +2,8 @@
 
 namespace cfg {
     namespace window {
-        const uint16_t width = 800;
-        const uint16_t height = 600;
+        const uint16_t width = 1280;
+        const uint16_t height = 720;
         const char* title = "rov-sim-env";
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,101 +3,198 @@
 #define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 #include "glad/glad.h"
-#include "spdlog/spdlog.h"
 #include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "spdlog/spdlog.h"
 
 #include <iostream>
 #include <stdlib.h>
 
-void glfwErrorCallback(int error, const char* description);
-void glfwWindowResizeCallback(GLFWwindow* window, int width, int height);
-void glfwKeyActionCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-void killAll(void);
+namespace lifetime {
+    inline void initSPDLOG();
+    inline void initGLFW();
+    inline void initIMGUI(GLFWwindow* window);
+    inline void killIMGUI();
+    
+    inline void killAll(uint8_t error);
+}
 
+namespace callbackGLFW {
+    void error(int error, const char* description);
+    void windowResize(GLFWwindow* window, int width, int height);
+    void keyAction(GLFWwindow* window, int key, int scancode, int action, int mods);
+}
+
+GLFWwindow* createWindow(int windowWidth, int windowHeight, const char* windowTitle) {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, windowTitle, NULL, NULL);
+    if (window == NULL) {
+        spdlog::error("GLFW couldn't create window.");
+        lifetime::killAll(1);
+    }
+    return window;
+}
 
 int main(int argc, char** args) {
 ///INIT SPDLOG
-    spdlog::set_level(spdlog::level::info);
+    lifetime::initSPDLOG();
     spdlog::info("Hello world, from spdlog.");
 
 
 ///INIT GLFW, SET ERROR CALLBACK
-    if(glfwInit() != GLFW_TRUE) {
-        spdlog::error("GLFW couldn't init.");
-        abort();
-    }
+    lifetime::initGLFW();
     spdlog::info("GLFW init.");
-    glfwSetErrorCallback(*glfwErrorCallback);
+    glfwSetErrorCallback((GLFWerrorfun)&callbackGLFW::error);
 
 
-///GET CONTEXT, SET CURRENT, SET STUFF
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    GLFWwindow* window = glfwCreateWindow(cfg::window::width, cfg::window::height, cfg::window::title, NULL, NULL);
-    if (window == NULL) {
-        spdlog::error("GLFW couldn't create window.");
-        killAll();
-    }
+///CREATE CONTEXT, SET CURRENT, SET STUFF
+    GLFWwindow* window = createWindow(cfg::window::width, cfg::window::height, cfg::window::title);
+    glfwSetWindowPos(window, 100, 100);
+
+    glfwSetWindowSizeLimits(window, cfg::window::width, cfg::window::height, GLFW_DONT_CARE, GLFW_DONT_CARE);
+
     glfwMakeContextCurrent(window);
     glfwSwapInterval(1);
 
 
 ///LOAD GL, FRAMEBUFFER SETUP
-    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    if(!gladLoadGLLoader((GLADloadproc)&glfwGetProcAddress)) {
         spdlog::error("GLAD couldn't load GLProc's");
-        killAll();
+        lifetime::killAll(1);
     }
-    static int framebufferInitialWidth, framebufferInitialHeight; 
-    glfwGetFramebufferSize(window, &framebufferInitialWidth, &framebufferInitialHeight);
-    glViewport(0, 0, framebufferInitialWidth - 100, framebufferInitialHeight - 100);
-    glfwSetFramebufferSizeCallback(window, (GLFWframebuffersizefun)&glfwWindowResizeCallback);
+    callbackGLFW::windowResize(window, 0, 0); //Call callback to set framebuffer size. Very smort...
 
 
-///SET INPUT CALLBACKS, 
-    glfwSetKeyCallback(window, (GLFWkeyfun)&glfwKeyActionCallback);
+///SET CALLBACKS FOR WINDOW
+    glfwSetFramebufferSizeCallback(window, (GLFWframebuffersizefun)&callbackGLFW::windowResize);
+    glfwSetKeyCallback(window, (GLFWkeyfun)&callbackGLFW::keyAction);
+
+
+///SETUP IMGUI (AND ITS BACKENDS)
+    lifetime::initIMGUI(window);
 
 
 ///GAME LOOP
-    while(!glfwWindowShouldClose(window)) {
+    while(1) {
+        ///OPENGL RENDER
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
+        ///IMGUI RENDER
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow(); 
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+
+        ///SWAP BUFFERS, POLL EVENTS
         glfwSwapBuffers(window);
         glfwPollEvents();
+
+        ///EXIT?
+        if(glfwWindowShouldClose(window)) {
+            break;
+        }
     }
 
 
-///DESTROY CONTEXT, TERMINATE
-    glfwDestroyWindow(window);
-    spdlog::info("GLFW terminating. [glfwTime : {0:.3f}(s)]", glfwGetTime());
-    glfwTerminate();
+///DESTROY IMGUI, (WINDOW & CONTEXT &) GLFW. THEN TERMINATE
+    lifetime::killAll(0);
     exit(EXIT_SUCCESS);
 }
 
-void glfwErrorCallback(int error, const char* description) {
-    spdlog::error("GLFW error.\n GLFW description start | {0} | GLFW description end", description);
-    killAll();
-}
-
-void glfwWindowResizeCallback(GLFWwindow* window, int width, int height) {
-    static int framebufferWidth, framebufferHeight;
-    glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
-    if(framebufferWidth <= 100 || framebufferHeight <= 100 ) {
-        spdlog::error("Framebuffer size fell below 100px.");
-        killAll();
+namespace callbackGLFW {
+    void error(int error, const char* description) {
+        spdlog::error("GLFW error.\n   >GLFW description start\n\n{0}\n\n   >GLFW description end", description);
+        lifetime::killAll(1);
     }
-    glViewport(0, 0, framebufferWidth - 100, framebufferHeight - 100);
-}
 
-void glfwKeyActionCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, 1);
+    void windowResize(GLFWwindow* window, int width, int height) {
+        static int framebufferWidth, framebufferHeight;
+        glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+        glViewport(0, 0, framebufferWidth, framebufferHeight);
+    }
+
+    void keyAction(GLFWwindow* window, int key, int scancode, int action, int mods) {
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+            glfwSetWindowShouldClose(window, 1);
+        }
     }
 }
 
-/// @brief Kills all. Logs an error, terminates GLFW, then aborts.
-inline void killAll(void) {
-    spdlog::error("You've done it again... Committing dead. [glfwTime : {0:.3f}(s)]", glfwGetTime());
-    glfwTerminate();
-    abort();
+namespace lifetime{
+    uint8_t IMGUIAlive; //Is IMGUI initialized
+    uint8_t GLFWAlive; //Is GLFW initialized
+    uint8_t SPDLOGAlive; //Is SPDLOG initialized
+
+    inline void initSPDLOG() {
+        spdlog::set_level(spdlog::level::info);
+        SPDLOGAlive = 1;
+    }
+
+    inline void initGLFW() {
+        if(glfwInit() != GLFW_TRUE) {
+            spdlog::error("GLFW couldn't init.");
+            lifetime::killAll(1);
+        }
+        else {
+            lifetime::GLFWAlive = 1;
+        }
+    }
+
+    inline void initIMGUI(GLFWwindow* window) {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        ImGui_ImplGlfw_InitForOpenGL(window, 1);
+        ImGui_ImplOpenGL3_Init();
+        IMGUIAlive = 1;
+    }
+
+    inline void killIMGUI() {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+        IMGUIAlive = 0;
+    }
+
+    /// @brief Kills all. Logs event with GLFW timestamp.
+    /// @param error If set to 1, logs error & aborts. If 0, logs info & DOESN'T ABORT.
+    inline void killAll(uint8_t error) {
+        if(GLFWAlive) { // Log Event
+            if(error) {
+                spdlog::error("You've done it again... Committing dead. [glfwTime : {0:.3f}(s)]", glfwGetTime());
+            } 
+            else {
+                spdlog::info("GLFW terminating. [glfwTime : {0:.3f}(s)]", glfwGetTime());
+            }
+        } 
+        else {
+            if(error) {
+                spdlog::error("You've done it again... Committing dead. [glfwTime : NOGLFW]");
+            } 
+        }
+
+        if(IMGUIAlive) {
+            killIMGUI();
+            IMGUIAlive = 0;
+        }
+        if(GLFWAlive) {
+            glfwTerminate();
+            GLFWAlive = 0;
+        }
+
+        if(error) {
+            abort();
+        }
+    }
 }
